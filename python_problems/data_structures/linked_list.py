986fe2ddb92ca05ebc4c5b7655ba232bbf1d8427
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2021-08-15 22:16:56 -0700

    Add a node on given position
    Signed-off-by: Anshuman Tripathi <anshuman.tripathi305@gmail.com>

--- python_problems/data_structures/linked_list.py
+++ python_problems/data_structures/linked_list.py
@@ -19,17 +19,38 @@ class LinkedList:
     def __init__(self, head: Node = None):
         self.head = head
 
-    def add(self, value):
+    def add(self, value, pos=None):
+        """
+        Add the given value to the LinkedList. By default a new value is added at the starting of the list.
+        :param value: Value to be added
+        :param pos: If this parameter is provided, add the node on the the specific position. All remaining nodes after
+                    provided position are shifted by one. The starting index of the linked list is 0 (zero).
+        """
         node = Node(value)
         if not self.head:
             # If there the list is empty set the new node as the head
             self.head = node
             return
+        elif pos is not None:
+            i = 0
+            prev_node = None
+            next_node = self.head
+            while pos != i:
+                i = i + 1
+                prev_node = next_node
+                next_node = next_node.next
+
+            prev_node.next = node
+            node.next = next_node
         else:
             node.next = self.head
             self.head = node
 
     def get_linked_list(self):
+        """
+        Get the list representation of the linked list
+        :return: a list of all the values of the nodes in the linked list. The order of the linked list is preserved.
+        """
         output = []
         node = self.head
         while node is not None:
--- python_problems/tests/test_linked_list.py
+++ python_problems/tests/test_linked_list.py
@@ -1,9 +1,15 @@
 from python_problems.data_structures.linked_list import LinkedList
 
 
-def test_linked_list():
+def test_linked_list_crud():
     linked_list = LinkedList()
+
     linked_list.add(1)
     linked_list.add(2)
     assert len(linked_list.get_linked_list()) != 0
     assert linked_list.get_linked_list() == [2, 1]
+
+    # Add "hello" at index 1
+    linked_list.add("hello", 1)
+
+    assert linked_list.get_linked_list() == [2, "hello", 1]
