fd7224f85cdae2a1de9e9dab0ac5cb2b017f722b
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2021-08-20 23:02:50 -0700

    Update formatting
    Signed-off-by: Anshuman Tripathi <anshuman.tripathi305@gmail.com>

--- src/test/java/problems/FirstDuplicateTest.java
+++ src/test/java/problems/FirstDuplicateTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class FirstDuplicateTest {
     private final FirstDuplicate firstDuplicate = new FirstDuplicate();
-    private final int[] input = new int[]{1,2,3,2,1};
+    private final int[] input = new int[]{1, 2, 3, 2, 1};
 
     @Test
     public void testBruteForce() {
