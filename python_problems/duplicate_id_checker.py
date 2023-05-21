1c928a050f48250b193b909e5a34856b06ad9f47
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2023-05-21 13:02:25 -0700

    Duplicate checker using bloom filter (#18)
    Signed-off-by: Anshuman Tripathi <anshuman.tripathi305@gmail.com>

---
+++ python_problems/duplicate_id_checker.py
@@ -0,0 +1,46 @@
+from typing import List
+import hashlib
+from threading import Lock
+
+class ProbabilisticDuplicateChecker:
+    def __init__(self, size, hashes):
+        """
+        Use bloom filter for a probabilistic check for duplicates
+        :param size: initial size of the bloom filter
+        :param hashes: Number of hashes used in the filter
+        """
+        self.size = size
+        self.hashes = hashes
+        self.bit_array = [False] * size
+        self._lock = Lock()
+
+    def add(self, value: any):
+        """
+        Record the existence of the given value in the Bloom filter.
+        :param value: Value to add the record of existence for in the bloom filter.
+        """
+        with self._lock:
+            for index in self._get_indices(value):
+                self.bit_array[index] = True
+
+    def __contains__(self, value: any) -> bool:
+        """
+        Override the contains method to check for existence of the given value in the bloom filter.
+        :param value: The value to make the existence check for
+        :return: True if the value exists otherwise return false
+        """
+        for index in self._get_indices(value):
+            return self.bit_array[index]
+
+    def _get_indices(self, value) -> List[int]:
+        """
+        Get the indices for a given value. The number of indices are equivalent to the number of hashes used in the
+        bloom filter
+        :param value: The value to check existence in the bloom filter.
+        :return: A list of indices in the bloom filter.
+        """
+        indices = []
+        for seed in range(self.hashes):
+            value_hash = hashlib.sha256(str(value).encode() + str(seed).encode()).digest()
+            indices.append(int.from_bytes(value_hash, byteorder='big') % self.size)
+        return indices
