1e4be3f738d3b03371d3a7609f81802b0a3970b1
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2024-01-13 21:45:38 -0800

    doc: authentication_manager. Add settings.json

--- python_problems/authentication_manager.py
+++ python_problems/authentication_manager.py
@@ -5,7 +5,6 @@ https://leetcode.com/problems/design-authentication-manager/description/
 
 
 class AuthenticationManager:
-
     def __init__(self, timeToLive: int):
         self.timeToLive = timeToLive
         self.tokens = {}
@@ -18,8 +17,11 @@ class AuthenticationManager:
             self.tokens[tokenId] = self.timeToLive + currentTime
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
+        # Since we have to remove elements in the dictionary while iterating over it, we need to interate over a list of tokens.
+        # Otherwise python would throw `RuntimeError: dictionary changed size during iteration`
         for tokenId in list(self.tokens):
             if self.tokens[tokenId] <= currentTime:
                 del self.tokens[tokenId]
 
         return len(self.tokens)
+    
